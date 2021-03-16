e571cd91b2eff4498c56de0ea2c03455df6fba88
aggelos24 <angelosarn@hotmail.com>
2021-03-16 23:50:41 +0200

    first commit, stocks route

---
+++ netlify-functions/stocks.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+const { parse } = require('node-html-parser');
+const pug = require('pug');
+
+const compiledFunction = pug.compileFile('templates/stocks.pug');
+
+
+exports.handler = async function(event, context) {
+	try {
+		const stockData = [];
+		const res = await axios.get('https://www.capital.gr/finance/realtimeticker2017');
+		const raw_html = res.data;
+		const root = parse(raw_html);
+		const tdElements = root.querySelectorAll('td');
+
+		tdElements.forEach((tdElement, index) => {
+			let temp = {};
+			if (tdElement.childNodes.length == 3) {
+				temp.symbol = tdElement.childNodes[1].innerText.trim();
+				temp.price = tdElements[index + 1].childNodes[0].textContent.trim();
+				temp.diff = tdElements[index + 2].childNodes[0].textContent.trim();
+				temp.percentage = tdElements[index + 3].childNodes[0].textContent.trim();
+
+				if (temp.percentage.includes('+')) {
+					temp.direction = 'up';	
+				} else if (temp.percentage.includes('-') && temp.percentage.length > 1) {
+					temp.direction = 'down';
+				} else {
+					temp.direction = 'noChange';
+				}
+
+				stockData.push(temp);
+			}
+		});
+
+		return {
+			statusCode: 200,
+			body: compiledFunction({
+				stocks: stockData
+			})
+		};
+	} catch (err) {
+		// Handle Error Here
+		console.error(err);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({message: "Something went wrong"})
+		};
+	}
+}
